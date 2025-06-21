93d2be1ff9ab52b4496dcf8ba8c1670671a272ac
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-21 11:25:14 -0300

    feat: add DeleteUserRepository

---
+++ src/repositories/postgres/deleteUser.js
@@ -0,0 +1,13 @@
+import { PostgresClient } from "../../db/postgres/client";
+("../../db/postgres/client.js");
+
+export class PostgresDeleteUserRepository {
+  async execute(userId) {
+    const deletedUser = await PostgresClient.query(
+      "DELETE FROM users WHERE id = $1 RETURNING *",
+      [userId]
+    );
+
+    return deletedUser[0];
+  }
+}
