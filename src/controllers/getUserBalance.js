1519c30aae2b82d4ff23cd83b19526d025a3a463
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-29 15:40:20 -0300

    feat:add GetUserBalanceController

---
+++ src/controllers/getUserBalance.js
@@ -0,0 +1,33 @@
+import { ok, serverError } from "../controllers/helpers/httpHelpers.js";
+import {
+  checkIfIdIsValid,
+  invalidIdResponse,
+  userNotFoundResponse,
+} from "./helpers/userHelpers.js";
+
+export class GetUserBalanceController {
+  constructor(getUserBalanceService) {
+    this.getUserBalanceService = getUserBalanceService;
+  }
+
+  async execute(httpRequest) {
+    try {
+      const userId = httpRequest.params.userId;
+      const idIsValid = checkIfIdIsValid(userId);
+
+      if (!idIsValid) {
+        return invalidIdResponse();
+      }
+
+      const balance = await this.getUserBalanceService.getBalance(userId);
+      return ok(balance);
+    } catch (error) {
+      if (error instanceof UserNotFoundError) {
+        return userNotFoundResponse();
+      }
+
+      console.error();
+      return serverError(error);
+    }
+  }
+}
