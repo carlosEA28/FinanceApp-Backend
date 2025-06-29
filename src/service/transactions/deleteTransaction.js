046c1505d5a248bc209a0be33472e8f5cc800574
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-29 14:34:29 -0300

    feat: add DeleteTransactionService

---
+++ src/service/transactions/deleteTransaction.js
@@ -0,0 +1,12 @@
+export class DeleteTransactionService {
+  constructor(deleteTransactionRepository) {
+    this.deleteTransactionRepository = deleteTransactionRepository;
+  }
+  async execute(transactionId) {
+    const transaction = await this.deleteTransactionRepository.execute(
+      transactionId
+    );
+
+    return transaction;
+  }
+}
