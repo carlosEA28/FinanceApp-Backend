f0bb2fb25707a27f362d92be87ce371e34a669ac
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-29 13:19:13 -0300

    feat: add UpdateTransactionRepository

--- src/repositories/postgres/transaction/updateTransaction.js
+++ src/repositories/postgres/transaction/updateTransaction.js
@@ -1,23 +1,24 @@
 import { PostgresClient } from "../../../db/postgres/client.js";
-export class PostgresUpdateUserRepository {
-  async execute(userId, updateUserParams) {
+
+export class PostgresUpdateTransactionRepository {
+  async execute(userId, updateTransactionParams) {
     const updatedFields = [];
     const updatedValues = [];
 
     Object.keys(updateUserParams).forEach((key, index) => {
       updatedFields.push(`${key} = $${index + 1}`);
-      updatedValues.push(updateUserParams[key]);
+      updatedValues.push(updateTransactionParams[key]);
     });
 
     updatedValues.push(userId);
     const userIdPlaceholder = `$${updatedValues.length}`;
 
     const updateQuery = `
-      UPDATE users
-      SET ${updatedFields.join(", ")}
-      WHERE id = ${userIdPlaceholder}
-      RETURNING *
-    `;
+         UPDATE transactions
+         SET ${updatedFields.join(", ")}
+         WHERE id = ${userIdPlaceholder}
+         RETURNING *
+       `;
 
     const result = await PostgresClient.query(updateQuery, updatedValues);
 
