6ffe058f85d8f867d6838a4db1056f7d1de87f71
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-29 13:37:15 -0300

    feat: add checkIfAmountIsValid validation

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -26,3 +26,11 @@ export const validateRequiredFields = (params, requiredFields) => {
     missingField: undefined,
   };
 };
+
+export const checkIfAmountIsValid = (amount) => {
+  validator.isCurrency(amount.toString(), {
+    digits_after_decimal: [2],
+    allow_negatives: false,
+    decimal_separator: ".",
+  });
+};
