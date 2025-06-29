0909b52f1c5828b50529168663eff595aeb9297a
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-29 14:31:54 -0300

    feat:add PostegresDeleteTransaction

---
+++ src/repositories/postgres/transaction/deleteTransaction.js
@@ -0,0 +1,12 @@
+import { PostgresClient } from "../../../db/postgres/client.js";
+
+export class PostgresDeleteTransaction {
+  async execute(transactionId) {
+    const transaction = await PostgresClient.query(
+      "DELETE FROM transactions WHERE id = $1 RETURNING *",
+      [transactionId]
+    );
+
+    return transaction[0];
+  }
+}
