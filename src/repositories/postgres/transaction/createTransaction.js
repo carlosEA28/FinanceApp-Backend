baf20f482ee59a09b9b2d5f104382e217a300e8b
carlosEA28 <carloseaduarte28@gmail.com>
2025-06-30 18:19:10 -0300

    feat:ensure Create and Delete transaction use prisma

--- src/repositories/postgres/transaction/createTransaction.js
+++ src/repositories/postgres/transaction/createTransaction.js
@@ -1,21 +1,9 @@
-import { PostgresClient } from "../../../db/postgres/client.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresCreateTransactioRepository {
   async execute(createTransactionParams) {
-    const createdTransaction = await PostgresClient.query(
-      `INSERT INTO transactions (id, user_id, name, date, amount, type) 
-       VALUES ($1,$2,$3,$4,$5,$6) 
-       RETURNING *`,
-      [
-        createTransactionParams.id,
-        createTransactionParams.user_id,
-        createTransactionParams.name,
-        createTransactionParams.date,
-        createTransactionParams.amount,
-        createTransactionParams.type,
-      ]
-    );
-
-    return createdTransaction[0];
+    return await prisma.transaction.create({
+      data: createTransactionParams,
+    });
   }
 }
--- src/repositories/postgres/transaction/deleteTransaction.js
+++ src/repositories/postgres/transaction/deleteTransaction.js
@@ -1,12 +1,12 @@
 import { PostgresClient } from "../../../db/postgres/client.js";
+import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresDeleteTransactionRepository {
   async execute(transactionId) {
-    const transaction = await PostgresClient.query(
-      "DELETE FROM transactions WHERE id = $1 RETURNING *",
-      [transactionId]
-    );
-
-    return transaction[0];
+    return await prisma.transaction.delete({
+      where: {
+        id: transactionId,
+      },
+    });
   }
 }
