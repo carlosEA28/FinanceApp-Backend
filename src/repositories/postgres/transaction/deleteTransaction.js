01c03c5ffe001e63da1011134fe4054c4531f10d
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-04 18:57:12 -0300

    feat: add zod to deleteTransactionController

--- src/repositories/postgres/transaction/deleteTransaction.js
+++ src/repositories/postgres/transaction/deleteTransaction.js
@@ -1,11 +1,24 @@
 import { prisma } from "../../../../prisma/prisma.js";
+import { TransactionNotFoundError } from "../../../errors/transaction.js";
+import { Prisma } from "../../../generated/prisma/index.js";
 
 export class PostgresDeleteTransactionRepository {
   async execute(transactionId) {
-    return await prisma.transaction.delete({
-      where: {
-        id: transactionId,
-      },
-    });
+    try {
+      return await prisma.transaction.delete({
+        where: {
+          id: transactionId,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        throw new TransactionNotFoundError(transactionId);
+      }
+      console.error(error);
+      throw error;
+    }
   }
 }
