be118d8cdc2dd48dc9e93e8513e3bab91562f185
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-04 21:54:36 -0300

    chore: add inital swagger setup

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import "dotenv/config.js";
-import express from "express";
-
-import { usersRouter } from "./src/routes/users.js";
-import { transactionsRouter } from "./src/routes/transactions.js";
-
-const app = express();
-app.use(express.json());
-
-app.use("/api/users", usersRouter);
-app.use("/api/transactions", transactionsRouter);
+import { app } from "./src/app.js";
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running on port ${process.env.PORT}`);
