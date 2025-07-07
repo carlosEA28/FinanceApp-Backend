a2cc45a1ecadce2b7951c4e3572861348d6799ef
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-07 20:58:25 -0300

    feat ensure GetUserBalance recives from and to params

--- src/service/user/getUserBalance.js
+++ src/service/user/getUserBalance.js
@@ -5,14 +5,18 @@ export class GetUserBalanceService {
     this.getUserBalanceRepository = getUserBalanceRepository;
     this.getUserByIdRepository = getUserByIdRepository;
   }
-  async execute(params) {
+  async execute(params, from, to) {
     const user = await this.getUserByIdRepository.execute(params.userId);
 
     if (!user) {
       throw new UserNotFoundError();
     }
 
-    const balance = await this.getUserBalanceRepository.execute(params.userId);
+    const balance = await this.getUserBalanceRepository.execute(
+      params.userId,
+      from,
+      to
+    );
 
     return balance;
   }
