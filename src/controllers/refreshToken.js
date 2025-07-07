7999474138e7c9b9cfb7da922df425cc7b65e2e1
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-07 18:56:16 -0300

    feat: add refreshTokenController

---
+++ src/controllers/refreshToken.js
@@ -0,0 +1,25 @@
+import { ok, serverError } from "./helpers/httpHelpers";
+import { UnauthorizedError } from "../errors/user.js";
+
+export class RefreshTokenController {
+  constructor(refreshTokenService) {
+    this.refreshTokenService = refreshTokenService;
+  }
+  execute(httpRequest) {
+    try {
+      const { refreshToken } = httpRequest.body;
+
+      const newToken = this.refreshTokenService.execute(refreshToken);
+
+      return ok(newToken);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof UnauthorizedError) {
+        return unauthorized();
+      }
+
+      return serverError();
+    }
+  }
+}
